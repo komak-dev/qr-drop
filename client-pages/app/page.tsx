6863d941b0167d64c2a4655be7b330c2c6ea4cb9
komak-dev <kouma.developer@gmail.com>
2025-03-23 01:23:43 +0900

    first commit

---
+++ client-pages/app/page.tsx
@@ -0,0 +1,64 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+
+export default function Page() {
+  return (
+    <Tabs defaultValue="upload" className="w-full h-full p-6">
+      <TabsList className="grid w-full grid-cols-2 w-[400px] mx-auto">
+        <TabsTrigger value="upload">Upload</TabsTrigger>
+        <TabsTrigger value="download">Download</TabsTrigger>
+      </TabsList>
+      <TabsContent value="upload">
+        <UploadScreen />
+      </TabsContent>
+      <TabsContent value="download">
+        <DownloadScreen />
+      </TabsContent>
+    </Tabs>
+  )
+}
+
+
+function UploadScreen() {
+
+  return (
+    <Card className="h-full">
+      upload
+      <QRCode />
+    </Card>
+  );
+}
+
+
+function DownloadScreen() {
+
+  return (
+    <Card className="h-full">
+      download
+    </Card>
+  );
+}
+
+
+function QRCode() {
+  return (
+    <div className="p-2">
+      <Button size="sm">
+        <p className="text-xs">Regenerate QR Code</p>
+      </Button>
+    </div>
+  );
+}
