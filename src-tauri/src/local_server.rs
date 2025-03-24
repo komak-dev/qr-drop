761b66fd2d6bed70d391c597b63c8e0bde1d8c00
komak-dev <kouma.developer@gmail.com>
2025-03-24 10:48:02 +0900

    feat: トークンを管理する構造体を作成

--- src-tauri/src/local_server.rs
+++ src-tauri/src/local_server.rs
@@ -11,6 +11,8 @@ use bytes::{Bytes, BytesMut};
 use std::io::Cursor;
 use std::io::Write;
 use zip::write::SimpleFileOptions;
+use std::time::{Duration, Instant};
+use rand::distr::{Alphanumeric, SampleString};
 
 use super::dev_configurator::{IS_CLIENT_PAGES_DEV, IS_API_DEV};
 
@@ -37,6 +39,65 @@ struct TFiles {
     d2m: Mutex<Vec<(String, Bytes)>>,
     m2d: Mutex<Vec<(String, Bytes)>>,
 }
+
+struct TokenManager {
+    d2m_token: Mutex<(Option<String>, Instant)>,
+    m2d_token: Mutex<(Option<String>, Instant)>,
+    expiration: Duration,
+}
+
+impl TokenManager {
+    fn new() -> Self {
+        TokenManager {
+            d2m_token: Mutex::new((None, Instant::now())),
+            m2d_token: Mutex::new((None, Instant::now())),
+            expiration: Duration::from_secs(10 * 60),
+        }
+    }
+
+    fn generate_token_string() -> String {
+        Alphanumeric.sample_string(&mut rand::rng(), 32)
+    }
+
+    fn set_d2m_token(&self) {
+        self.d2m_token.lock().unwrap().0 = Some(Self::generate_token_string());
+        self.d2m_token.lock().unwrap().1 = Instant::now();
+    }
+
+    fn set_m2d_token(&self) {
+        self.m2d_token.lock().unwrap().0 = Some(Self::generate_token_string());
+        self.m2d_token.lock().unwrap().1 = Instant::now();
+    }
+
+    fn get_d2m_remaining_secs(&self) -> u64 {
+        let token = self.d2m_token.lock().unwrap();
+        if token.0.is_none() || token.1.elapsed() > self.expiration {
+            0
+        } else {
+            self.expiration.as_secs() - token.1.elapsed().as_secs()
+        }
+    }
+
+    fn get_m2d_remaining_secs(&self) -> u64 {
+        let token = self.m2d_token.lock().unwrap();
+        if token.0.is_none() || token.1.elapsed() > self.expiration {
+            0
+        } else {
+            self.expiration.as_secs() - token.1.elapsed().as_secs()
+        }
+    }
+
+    fn validate_d2m_token(&self, t: &str) -> bool {
+        let token = self.d2m_token.lock().unwrap();
+        token.0.is_some() && token.0.as_ref().unwrap() == t
+    }
+
+    fn validate_m2d_token(&self, t: &str) -> bool {
+        let token = self.m2d_token.lock().unwrap();
+        token.0.is_some() && token.0.as_ref().unwrap() == t
+    }
+}
+
 
 
 
